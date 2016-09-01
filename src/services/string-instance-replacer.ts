ba80209d958812fcae77b01048761fc8fbac1172
bombadillo <mumfordnsons@gmail.com>
2016-09-01 11:47:31 +0100

    Update to enable processing of multiple asset referencing files (e.g. index.html, app.component.ts

--- src/services/string-instance-replacer.ts
+++ src/services/string-instance-replacer.ts
@@ -1,30 +1,61 @@
 import { FileReader } from './file-reader';
+import { FileWriter } from './file-writer';
 import { AssetNameReplacer } from './asset-name-replacer';
 
 export class StringInstanceReplacer {
 
   fileReader: FileReader;
+  fileWriter: FileWriter;
   assetNameReplacer: AssetNameReplacer;
+
+  filesToReplace;
 
   constructor() {
     this.fileReader = new FileReader();
+    this.fileWriter = new FileWriter();
     this.assetNameReplacer = new AssetNameReplacer();
   }
 
-  replaceInstances = (fileToModify, files) => {    
+  replaceInstances = (files) => {
+    this.filesToReplace = files;
+    var filesToReplace = ['src/test/index.html', 'src/test/app/app.component.ts'];
+    return new Promise((fulfill, reject) => {
+      var actions = filesToReplace.map(this.replaceInstance);
+      var results = Promise.all(actions);
+
+      results.then(data => {
+        console.log('updated files');
+        fulfill(data);
+      });       
+    });      
+  }
+
+  replaceInstance = (fileToModify) => {    
     return new Promise((fulfill, reject) => {
       this.fileReader.readFile(fileToModify)
         .then((fileContents: string) => {
-          for (let file of files) {
-            fileContents = this.assetNameReplacer.replace(
-              fileContents, 
-              file.originalFileName, 
-              file.versionedFileName
-            );
-          }    
-
-          fulfill(fileContents);
+          return this.iterateFiles(fileContents);
+        })
+        .then((fileContents) => {
+          console.log(`updating file ${fileToModify}`);
+          this.fileWriter.write(fileToModify, fileContents).then(() => {
+            fulfill();
+          });             
         });
     });
   }  
+
+  iterateFiles = (fileContents) => {
+    console.log('iterating files')
+    return new Promise((fulfill, reject) => {
+      for (let file of this.filesToReplace) {
+        fileContents = this.assetNameReplacer.replace(
+          fileContents, 
+          file.originalFileName, 
+          file.versionedFileName
+        );
+      }    
+      fulfill(fileContents);
+    });    
+  }
 }
--- src/services/versioner.ts
+++ src/services/versioner.ts
@@ -1,26 +1,20 @@
 import { FileRetriever } from './file-retriever';
 import { FileNameParser } from './filename-parser';
-import { FileWriter } from './file-writer';
 import { FileRenamer } from './file-renamer';
 import { StringInstanceReplacer} from './string-instance-replacer';
 
 export class Versioner {
   
   fileRetriever: FileRetriever;
-  fileNameParser: FileNameParser;
-  fileWriter: FileWriter;
+  fileNameParser: FileNameParser;  
   fileRenamer: FileRenamer;
   stringInstanceReplacer: StringInstanceReplacer;
 
   parsedFiles;
-  fileContents;
-
-  assetReferencingFile = 'src/test/index.html';
 
   constructor() {
     this.fileRetriever = new FileRetriever();    
-    this.fileNameParser = new FileNameParser();    
-    this.fileWriter = new FileWriter();
+    this.fileNameParser = new FileNameParser();        
     this.fileRenamer = new FileRenamer();
     this.stringInstanceReplacer = new StringInstanceReplacer();
   }
@@ -30,11 +24,7 @@ export class Versioner {
       this.fileRetriever.retrieve(fileSearch)
         .then((files: string[]) => {    
           this.parsedFiles = this.fileNameParser.parse(files);      
-          return this.stringInstanceReplacer.replaceInstances(this.assetReferencingFile, this.parsedFiles);                                          
-        })
-        .then((fileContents) => {
-          this.fileContents = fileContents;
-          return this.fileWriter.write(this.assetReferencingFile, fileContents);             
+          return this.stringInstanceReplacer.replaceInstances(this.parsedFiles);                                          
         })
         .then(() => {
           var actions = this.parsedFiles.map(this.fileRenamer.rename);
@@ -42,7 +32,7 @@ export class Versioner {
 
           results.then(data => {
             console.log(data)
-            fulfill(this.fileContents);
+            fulfill(true);
           });             
         });      
     });    
